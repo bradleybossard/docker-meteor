1c975df745d18930871621b30bcccc8b86c959c2
Bradley Bossard <bradleybossard@gmail.com>
2015-12-27 17:11:27 +0000

    Added functionality for adding users, removing users and giving points to users.

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,12 +1,50 @@
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find()
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
     },
-    'otherHelperFunction': function(){
-      return PlayersList.find()
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer)
     }
   });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+        var playerId = this._id;
+        Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      console.log('point1');
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
+      event.target.playerName.value = '';
+    }
+  });
+
 
 /*
   // counter starts at 0
